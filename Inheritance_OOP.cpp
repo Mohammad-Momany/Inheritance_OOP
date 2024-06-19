fe08becb5155c18cb6ae11f31c8d801fac904594
Mohammad Almomany <mohammad.w.momani@gmail.com>
2024-06-19 19:47:48 +0300

    Implement the virtual function overriding with pointers up casting.

--- Inheritance_OOP.cpp
+++ Inheritance_OOP.cpp
@@ -248,9 +248,15 @@ int main()
 
 {
 
+    clsEmployee Employee(10, "Mohammed", "Almomany", "A@a.com", "8298982", "Med-Engineer", "Engineering", 3000);
     clsDeveloper Developer(10, "Mohammed", "Almomany", "A@a.com", "8298982", "Med-Engineer", "Engineering", 3000, "C++");
+  
 
-    Developer.Print();
+    clsPerson* Person1 = &Employee;
+    clsPerson* Person2 = &Developer;
+
+    Person1->Print();
+    Person2->Print();
 
 
     system("pause>0");
